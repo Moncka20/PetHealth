c905b6a660a706d9ee9a30435d7bf4b9100d043b
Stiven <saortega24@itp.edu.co>
2026-03-18 22:17:20 -0500

    feat: Implement appointment scheduling and treatment management features including entities, module, and service.

--- src/modules/agendamiento.module.ts
+++ src/modules/agendamiento.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Agendamiento } from '../entities/agendamiento.entity';
+import { Agendamiento } from '../entities/agendamiento/agendamiento.entity';
 import { AgendamientoService } from '../services/agendamiento.service';
 import { AgendamientoController } from '../controllers/agendamiento.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Agendamiento])],
-  controllers: [AgendamientoController],
-  providers: [AgendamientoService],
-  exports: [AgendamientoService],
+    imports: [TypeOrmModule.forFeature([Agendamiento])],
+    controllers: [AgendamientoController],
+    providers: [AgendamientoService],
+    exports: [AgendamientoService],
 })
-export class AgendamientoModule {}
+export class AgendamientoModule { }
