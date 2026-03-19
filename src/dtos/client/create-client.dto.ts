926a5bfdec2263909323ef543d61227b587402f4
DANI <dannyandrestecnologia@outlook.com>
2026-03-18 20:16:04 -0500

    Validacion de DTO y entity de Client

--- src/dtos/client/create-client.dto.ts
+++ src/dtos/client/create-client.dto.ts
@@ -1,1 +1,29 @@
-export class CreateClientDto {}
+import { IsString, IsEmail, IsOptional } from 'class-validator'
+
+
+export class CreateClientDto {
+    @IsString()
+    first_name: string;
+
+    @IsString()
+    @IsOptional()
+    last_name: string;
+
+    @IsString()
+    document_id: string;
+
+    @IsString()
+    address: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    @IsOptional()
+    phone?: string;
+
+    @IsString()
+    @IsOptional()
+    notes: string;
+
+}
--- src/entities/client/client.entity.ts
+++ src/entities/client/client.entity.ts
@@ -1,1 +1,30 @@
-export class Client {}
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+
+@Entity()
+export class Client {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  first_name: string;
+
+  @Column()
+  last_name: string;
+
+  @Column({ unique: true })
+  document_id: string;
+
+  @Column()
+  address: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  phone: string;
+
+  @Column({ nullable: true })
+  notes: string;
+}
